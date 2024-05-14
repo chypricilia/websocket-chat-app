9f3ed66eff300ddbde030d73f0fe36bca30a6396
chypricilia <chypricilianna@yahoo.co.id>
2024-05-14 18:37:32 +0700

    first commit

---
+++ src/main/java/org/example/websocketchatapp/WebsocketChatAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.websocketchatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketChatAppApplication.class, args);
+	}
+
+}
