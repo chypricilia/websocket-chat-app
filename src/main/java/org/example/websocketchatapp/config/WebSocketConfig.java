999834855d7faee04a426dc0465502d80c75fc79
chypricilia <chypricilianna@yahoo.co.id>
2024-05-14 18:40:08 +0700

    create websocket config

---
+++ src/main/java/org/example/websocketchatapp/config/WebSocketConfig.java
@@ -0,0 +1,35 @@
+/**
+ * Author: Pricilia Anna V
+ * Date:5/14/2024
+ * Time:6:38 PM
+ */
+
+package org.example.websocketchatapp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+/**
+ * Author: Pricilia Anna V
+ * Date:5/14/2024
+ * Time:6:38 PM
+ */
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS();
+    }
+    
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
+    }
+}
